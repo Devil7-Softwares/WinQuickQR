924536ff6bac303823a99ae1b5d186700c58b577
Devil7DK <dineshthangavel47@gmail.com>
2023-11-29 20:28:36 +0530

    Update assembly & publish information

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WinQuickQR")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Quick & accessible QR Code generator for Windows")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Devil7 Softwares")]
 [assembly: AssemblyProduct("WinQuickQR")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright © Devil7 Softwares 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
